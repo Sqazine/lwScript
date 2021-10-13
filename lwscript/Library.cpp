5e2be4ed3703d8e9fc2b7df0aef71090a6a0fbdc
Sqazine Chan <Sqazine@163.com>
2021-10-13 21:47:44 +0800

    add more native functions

--- lwscript/Library.cpp
+++ lwscript/Library.cpp
@@ -1,5 +1,6 @@
 #include "Library.h"
 #include "VM.h"
+#include <stdio.h>
 namespace lws
 {
 
@@ -39,34 +40,58 @@ namespace lws
     IO::IO(VM *vm)
         : Library(vm)
     {
-        m_NativeFunctions["println"] = [this](std::vector<Object *> args) -> Object *
-        {
-            if (args.empty())
-                return nullptr;
+		m_NativeFunctions["print"] = [this](std::vector<Object*> args) -> Object*
+		{
+			if (args.empty())
+				return nullptr;
 
-            if (args[0]->Type() != ObjectType::STR)
-                Assert("Invalid argument:The first argument of native print function must be string type.");
-            if (args.size() == 1)
-                std::cout << args[0]->Stringify() << std::endl;
-            else //formatting output
+			if (args[0]->Type() != ObjectType::STR)
+				Assert("[native function 'println']:Invalid argument:The first argument must be string type.");
+
+
+			std::string content = TO_STR_OBJ(args[0])->value;
+
+			if (args.size() != 1)//formatting output
             {
-                std::string content = TO_STR_OBJ(args[0])->value;
+				int32_t pos = (int32_t)content.find("{}");
+				int32_t argpos = 1;
+				while (pos != std::string::npos)
+				{
+					if (argpos < args.size())
+						content.replace(pos, 2, args[argpos++]->Stringify());
+					else
+						content.replace(pos, 2, "nil");
+					pos = content.find("{}");
+				}
+			}
 
-                int32_t pos = (int32_t)content.find("{}");
-                int32_t argpos = 1;
-                while (pos != std::string::npos)
-                {
-                    if (argpos < args.size())
-                        content.replace(pos, 2, args[argpos++]->Stringify());
-                    else
-                        content.replace(pos, 2, "nil");
-                    pos = content.find("{}");
-                }
+			int32_t pos = (int32_t)content.find("\\n");
+			while (pos != std::string::npos)
+			{
+				content[pos]='\n';
+                content.replace(pos+1, 1, "");//erase a char
+				pos = content.find("\\n");
+			}
 
-                std::cout << content << std::endl;
-            }
-            return nullptr;
-        };
+			pos = (int32_t)content.find("\\t");
+			while (pos != std::string::npos)
+			{
+				content[pos] = '\t';
+				content.replace(pos + 1, 1, "");//erase a char
+				pos = content.find("\\t");
+			}
+
+			pos = (int32_t)content.find("\\r");
+			while (pos != std::string::npos)
+			{
+				content[pos] = '\r';
+				content.replace(pos + 1, 1, "");//erase a char
+				pos = content.find("\\r");
+			}
+
+            std::cout << content;
+			return nullptr;
+		};
     }
 
     DataStructure::DataStructure(VM *vm)
@@ -87,5 +112,105 @@ namespace lws
                 Assert("[Native function 'sizeof']:Expect a array,table ot string argument.");
             return nullptr;
         };
+
+        m_NativeFunctions["insert"] = [this](std::vector<Object*> args)->Object*
+        {
+			if (args.empty() || args.size() !=3)
+				Assert("[Native function 'insert']:Expect 3 arguments,the arg0 must be array,table or string object.The arg1 is the index object.The arg2 is the value object.");
+
+            if (IS_ARRAY_OBJ(args[0]))
+            {
+                ArrayObject* array = TO_ARRAY_OBJ(args[0]);
+                if (!IS_INTEGER_OBJ(args[1]))
+                    Assert("[Native function 'insert']:Arg1 must be integer type while insert to a array");
+
+                int64_t iIndex = TO_INTEGER_OBJ(args[1])->value;
+
+                if(iIndex<0||iIndex>=array->elements.size())
+                    Assert("[Native function 'insert']:Index out of array's range");
+
+                array->elements.insert(array->elements.begin()+iIndex,1,args[2]);
+            }
+            else if (IS_TABLE_OBJ(args[0]))
+            {
+				TableObject* table = TO_TABLE_OBJ(args[0]);
+			
+                for (auto [key, value] : table->elements)
+                    if (key->IsEqualTo(args[1]))
+                        Assert("[Native function 'insert']:Already exist value in the table object of arg1"+args[1]->Stringify());
+
+                table->elements[args[1]]=args[2];
+            }
+            else if (IS_STR_OBJ(args[0]))
+            {
+				StrObject* string = TO_STR_OBJ(args[0]);
+				if (!IS_INTEGER_OBJ(args[1]))
+					Assert("[Native function 'insert']:Arg1 must be integer type while insert to a array");
+
+				int64_t iIndex = TO_INTEGER_OBJ(args[1])->value;
+
+				if (iIndex < 0 || iIndex >= string->value.size())
+					Assert("[Native function 'insert']:Index out of array's range");
+
+				string->value.insert(iIndex,args[2]->Stringify());
+            }
+			else
+				Assert("[Native function 'insert']:Expect a array,table ot string argument.");
+			return nullptr;
+        };
+
+
+		m_NativeFunctions["erase"] = [this](std::vector<Object*> args)->Object*
+		{
+			if (args.empty() || args.size() != 2)
+				Assert("[Native function 'erase']:Expect 2 arguments,the arg0 must be array,table or string object.The arg1 is the corresponding index object.");
+
+			if (IS_ARRAY_OBJ(args[0]))
+			{
+				ArrayObject* array = TO_ARRAY_OBJ(args[0]);
+				if (!IS_INTEGER_OBJ(args[1]))
+					Assert("[Native function 'erase']:Arg1 must be integer type while insert to a array");
+
+				int64_t iIndex = TO_INTEGER_OBJ(args[1])->value;
+
+				if (iIndex < 0 || iIndex >= array->elements.size())
+					Assert("[Native function 'erase']:Index out of array's range");
+
+				array->elements.erase(array->elements.begin()+iIndex);
+			}
+			else if (IS_TABLE_OBJ(args[0]))
+			{
+				TableObject* table = TO_TABLE_OBJ(args[0]);
+
+				bool hasValue = false;
+
+				for (auto it = table->elements.begin(); it != table->elements.end(); ++it)
+					if (it->first->IsEqualTo(args[1]))
+					{
+						table->elements.erase(it);
+						hasValue = true;
+						break;
+					}
+
+				if (!hasValue)
+					Assert("[Native function 'erase']:No corresponding index in table.");
+			}
+			else if (IS_STR_OBJ(args[0]))
+			{
+				StrObject* string = TO_STR_OBJ(args[0]);
+				if (!IS_INTEGER_OBJ(args[1]))
+					Assert("[Native function 'erase']:Arg1 must be integer type while insert to a array");
+
+				int64_t iIndex = TO_INTEGER_OBJ(args[1])->value;
+
+				if (iIndex < 0 || iIndex >= string->value.size())
+					Assert("[Native function 'erase']:Index out of array's range");
+
+				string->value.erase(string->value.begin()+iIndex);
+			}
+			else
+				Assert("[Native function 'erase']:Expect a array,table ot string argument.");
+			return nullptr;
+		};
     }
 }
